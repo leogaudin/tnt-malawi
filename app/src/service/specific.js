944f8bc9f3bc9ece6e5b393306b0879f395032f8
Léo <leogaudinoff@gmail.com>
2024-11-01 18:19:56 +0100

    feat: add customization

--- app/src/service/specific.js
+++ app/src/service/specific.js
@@ -1,10 +1,10 @@
 // Look in /client/src/service/specific.js for the colors object
 export const colors = {
-	lightest: '#F0F5FF',
-	light: '#EBF0FE',
-	main: '#0949FF',
-	dark: '#0033C0',
-	darkest: '#001D6A',
+	lightest: '#FFE7E6',
+	light: '#F5898B',
+	main: '#CE1126',
+	dark: '#A8051E',
+	darkest: '#5C0014',
 };
 
-export const API_URL = 'https://track-and-trace-api.vercel.app/api';
+export const API_URL = 'https://tnt-malawi-api.vercel.app/api';
